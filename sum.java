f82c88b5285eb6678ae02e07b690b111742ab8c4
Rohit Prasad <golujsr@gmail.com>
2023-03-15 16:06:12 +0530

    Calculate Sum of Two Number

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args)
+    {
+        Scanner aa = new Scanner(System.in);
+        int a;
+        int b;
+        a = aa.nextInt();
+        b = aa.nextInt();
+       int c =a + b;
+        System.out.println("a+b" + "=" + c);
+    }
+}
